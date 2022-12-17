323454ff819d381186f3ad10fe5248104d56a457
“spica-jp” <spica8191@gmail.com>
2022-12-17 23:09:33 +0900

    solve ABC282-A by spica

---
+++ AtCoder/ABC/A/282/a_spica.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int K;
+    cin >> K;
+
+    for (int i = 0; i < K; i++) {
+        char moji = 0x41 + i;
+        cout << moji;
+    }
+    cout << endl;
+}
