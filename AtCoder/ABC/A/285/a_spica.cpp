ea7833b465c15a2e6682fc5739891ceb465aecae
“spica-jp” <spica8191@gmail.com>
2023-01-19 02:57:05 +0900

    solve ABC285-A

---
+++ AtCoder/ABC/A/285/a_spica.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (b == 2 * a || b == 2 * a + 1) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
