9b8304036bf3521681c583a0667954a8055f7fc5
Yasuaki <yasuakimizuno36@gmail.com>
2023-03-09 17:55:37 +0900

    solve Typical/001

---
+++ AtCoder/Typical90/001/001.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(ll i=0; i < ll(n); i++)
+#define nrep(i, n) for(ll i=1; i<ll(n+1); i++)
+#define rrep(i, n) for(ll i=ll(n)-1; i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).end(), (x).begin()
+#define SIZE 2e5
+constexpr long long MOD = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+typedef vector<ll> iv;
+typedef vector<bool> bv;
+typedef pair<ll, ll> ip;
+typedef vector<ip> pv;
+typedef vector<iv> ivv;
+ll gcd(ll a, ll b){
+    return b ? gcd(b, a%b) : a;
+}
+
+ll lcm(ll a, ll b){
+    return a / gcd(a, b) * b;
+}
+
+bool is_prime(ll x){
+    if(x < 2) return 0;
+    else if(x == 2) return 1;
+    if(x%2 == 0) return 0;
+    for(ll i=0; i<x; i++) if(x%i == 0) return 0;
+    return 0;
+}
+
+ll digsum(ll n){
+    ll res = 0;
+    while(n > 0){
+        res += n%10;
+        n /= 10;
+    }
+    return res;
+}
+
+ll pow(ll x, ll n){
+    ll res=1; while(n--) res *= x;
+    return res;
+}
+
+struct UnionFind{
+    iv par, rank, siz, min_node;
+    UnionFind(int n) : par(n, -1), rank(n, 0), siz(n, 1) {}
+    
+    int root(int x){
+        if(par.at(x) == -1) return x;
+        else return par.at(x) = root(par.at(x));
+    }
+    bool is_same(int x, int y){
+        return root(x) == root(y);
+    }
+    bool unite(int x, int y){
+        int rx = root(x), ry = root(y);
+        if(is_same(x, y)) return false;
+        if(siz.at(rx) < siz.at(ry)) swap(rx, ry);
+        par.at(ry) = rx;
+        if(rank.at(rx) == rank.at(ry)) rank.at(rx) += 1;
+        siz.at(rx) += siz.at(ry);
+        return true;
+    }
+};
+
+int how_many(int x, iv &a){   
+    int n = a.size();
+    int len = 0, cnt = 0;
+    rep(i, n){
+        len += a.at(i);
+        if(len >= x){
+            ++cnt;
+            len = 0;
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    int n, l, k; cin >> n >> l >> k;
+    iv a(n); rep(i, n) cin >> a.at(i);
+
+    // 羊羹の長さの配列を作成
+    iv len(n+1); 
+    len.at(0) = a.at(0);
+    len.at(n) = l - a.at(n-1);
+    rep(i, n-1) len.at(i+1) = a.at(i+1) - a.at(i);
+
+    int left = -1, right = l + 1;
+    while(right - left > 1){
+        int mid = (left + right) / 2;
+        //cout << mid << ' ' << how_many(mid, len) << endl;
+        if(how_many(mid, len) >= k+1) left = mid;
+        else right = mid;
+    }
+    cout << left << endl;
+}
