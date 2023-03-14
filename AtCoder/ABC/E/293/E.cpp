1cfa8eda3e6d5ebe87ee76a4148a5c698fb8aba5
Yasuaki <yasuakimizuno36@gmail.com>
2023-03-14 15:27:00 +0900

    solve 293/E

---
+++ AtCoder/ABC/E/293/E.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(ll i=0; i < ll(n); i++)
+#define nrep(i, n) for(ll i=1; i<ll(n+1); i++)
+#define rrep(i, n) for(ll i=ll(n)-1; i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).end(), (x).begin()
+#define SIZE 2e5
+constexpr long long MOD = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+typedef vector<ll> iv;
+typedef vector<bool> bv;
+typedef pair<ll, ll> ip;
+typedef vector<ip> pv;
+typedef vector<iv> ivv;
+ll gcd(ll a, ll b){
+    return b ? gcd(b, a%b) : a;
+}
+
+ll lcm(ll a, ll b){
+    return a / gcd(a, b) * b;
+}
+
+bool is_prime(ll x){
+    if(x < 2) return 0;
+    else if(x == 2) return 1;
+    if(x%2 == 0) return 0;
+    for(ll i=0; i<x; i++) if(x%i == 0) return 0;
+    return 0;
+}
+
+ll digsum(ll n){
+    ll res = 0;
+    while(n > 0){
+        res += n%10;
+        n /= 10;
+    }
+    return res;
+}
+
+ll pow(ll x, ll n){
+    ll ans = 1;
+    while(n){
+        if(n % 2) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+
+ivv mat_mul(ivv a, ivv b, ll mod){
+    int n = a.size();
+    ivv res(n, iv(n));
+    rep(i, n){
+        rep(j, n){
+            rep(k, n){
+                res.at(i).at(j) += a.at(i).at(k) * b.at(k).at(j);
+                res.at(i).at(j) %= mod;
+            }
+        }
+    }
+    return res;
+}
+
+ivv mat_pow(ivv a, ll b, ll mod){
+    int n = a.size();
+    ivv res(n, iv(n));
+    rep(i, n) res.at(i).at(i) = 1;
+    while(b){
+        if(b & 1) res = mat_mul(res, a, mod);
+        a = mat_mul(a, a, mod);
+        b >>= 1;
+    }
+    return res;
+}
+
+int main(){
+    ll a, x, m; cin >> a >> x >> m;
+    ivv res = {{a, 1}, {0, 1}};
+    res = mat_pow(res, x, m);
+    cout << res.at(0).at(1) % m << endl;
+}
