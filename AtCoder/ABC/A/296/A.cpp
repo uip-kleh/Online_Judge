ab086c3e793175179c9ba133c5ea6eac119ebf5a
Yasuaki <yasuakimizuno36@gmail.com>
2023-04-02 10:28:48 +0900

    solve 296

---
+++ AtCoder/ABC/A/296/A.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(ll i=0; i < ll(n); i++)
+#define nrep(i, n) for(ll i=1; i<ll(n+1); i++)
+#define rrep(i, n) for(ll i=ll(n)-1; i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).end(), (x).begin()
+#define SIZE 2e5
+constexpr long long MOD = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+typedef vector<ll> iv;
+typedef vector<bool> bv;
+typedef pair<ll, ll> ip;
+typedef vector<ip> pv;
+typedef vector<iv> ivv;
+ll gcd(ll a, ll b){
+    return b ? gcd(b, a%b) : a;
+}
+
+ll lcm(ll a, ll b){
+    return a / gcd(a, b) * b;
+}
+
+bool is_prime(ll x){
+    if(x < 2) return 0;
+    else if(x == 2) return 1;
+    if(x%2 == 0) return 0;
+    for(ll i=0; i<x; i++) if(x%i == 0) return 0;
+    return 0;
+}
+
+ll digsum(ll n){
+    ll res = 0;
+    while(n > 0){
+        res += n%10;
+        n /= 10;
+    }
+    return res;
+}
+
+ll pow(ll x, ll n){
+    ll ans = 1;
+    while(n){
+        if(n % 2) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+
+int main(){
+    int n; cin >> n;
+    string s; cin >> s;
+    if(n == 1){
+        cout << "Yes" << endl;
+        return 0;
+    }
+    rep(i, n-1){
+        if(s.at(i) == s.at(i+1)){
+            cout << "No" << endl;
+            return 0;
+        }
+    }
+    cout << "Yes" << endl;
+}
---
+++ AtCoder/ABC/D/296/D.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(ll i=0; i < ll(n); i++)
+#define nrep(i, n) for(ll i=1; i<ll(n+1); i++)
+#define rrep(i, n) for(ll i=ll(n)-1; i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).end(), (x).begin()
+#define SIZE 2e5
+constexpr long long MOD = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+typedef vector<ll> iv;
+typedef vector<bool> bv;
+typedef pair<ll, ll> ip;
+typedef vector<ip> pv;
+typedef vector<iv> ivv;
+ll gcd(ll a, ll b){
+    return b ? gcd(b, a%b) : a;
+}
+
+ll lcm(ll a, ll b){
+    return a / gcd(a, b) * b;
+}
+
+bool is_prime(ll x){
+    if(x < 2) return 0;
+    else if(x == 2) return 1;
+    if(x%2 == 0) return 0;
+    for(ll i=0; i<x; i++) if(x%i == 0) return 0;
+    return 0;
+}
+
+ll digsum(ll n){
+    ll res = 0;
+    while(n > 0){
+        res += n%10;
+        n /= 10;
+    }
+    return res;
+}
+
+ll pow(ll x, ll n){
+    ll ans = 1;
+    while(n){
+        if(n % 2) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+
+int main(){
+    ll n, m; cin >> n >> m;
+    ll ans = 2e18;
+    for(ll i=1; i<=n; i++){
+        ll x = (m+i-1) / i;
+        // cout << i << 'x' << x << '=' << i*x << endl;
+        if(x <= n) ans = min(ans, i*x);
+        if(i > x) break;
+    }
+    cout << (ans == 2e18 ? -1 : ans) << endl;
+}
