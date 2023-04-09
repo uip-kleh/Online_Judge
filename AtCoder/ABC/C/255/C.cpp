2380235a8659ecc694a4d07ccd7c3683aeb16dab
Yasuaki <yasuakimizuno36@gmail.com>
2023-04-09 17:45:33 +0900

    solve ABC/255/C

---
+++ AtCoder/ABC/C/255/C.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(ll i=0; i < ll(n); i++)
+#define nrep(i, n) for(ll i=1; i<ll(n+1); i++)
+#define rrep(i, n) for(ll i=ll(n)-1; i>=0; i--)
+#define all(x) (x).begin(), (x).end()
+#define SIZE 2e5
+#define INF 2e18
+constexpr long long MOD = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+typedef vector<ll> iv;
+typedef vector<bool> bv;
+typedef pair<ll, ll> ip;
+typedef vector<ip> pv;
+typedef vector<iv> ivv;
+ll gcd(ll a, ll b){
+    return b ? gcd(b, a%b) : a;
+}
+
+ll lcm(ll a, ll b){
+    return a / gcd(a, b) * b;
+}
+
+bool is_prime(ll x){
+    if(x < 2) return 0;
+    else if(x == 2) return 1;
+    if(x%2 == 0) return 0;
+    for(ll i=0; i<x; i++) if(x%i == 0) return 0;
+    return 0;
+}
+
+ll digsum(ll n){
+    ll res = 0;
+    while(n > 0){
+        res += n%10;
+        n /= 10;
+    }
+    return res;
+}
+
+ll pow(ll x, ll n){
+    ll ans = 1;
+    while(n){
+        if(n % 2) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+iv dx = {-1, 0, 1, 0};
+iv dy = {0, -1, 0, 1};
+
+struct edge {
+    int from, to, length;
+};
+
+int main(){
+    ll x, a, d, n; cin >> x >> a >> d >> n;
+    if(d < 0){
+        d = abs(d);
+        a -= d * (n-1);
+    }
+    ll l = 0, r = n-1;
+    while(r - l > 1){
+        ll mid = (l + r) / 2;
+        if(a + d * mid >= x) r = mid;
+        else l = mid;
+    }
+    ll ans = INF;
+    cout << min(abs(x-(a + d*l)), abs(x-(a + d*r))) << endl;
+}
