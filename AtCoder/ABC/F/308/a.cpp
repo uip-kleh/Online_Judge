a4c0301cd9fb966dced903efa3a4a61399682862
uip-kleh <h23ms416@hirosaki-u.ac.jp>
2023-07-15 19:17:35 +0900

    solve

---
+++ AtCoder/ABC/F/308/a.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(ll i = 0; i < ll(n); i++)
+#define nrep(i, n) for(ll i = 1; i < ll(n + 1); i++)
+#define rrep(i, n) for(ll i = ll(n) - 1; i >= 0; i--)
+#define all(x) (x).begin(), (x).end()
+#define SIZE 2e5
+#define INF 2e18
+constexpr long long MOD = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+typedef vector<int> iv;
+typedef vector<ll> lv;
+typedef vector<bool> bv;
+typedef pair<ll, ll> ip;
+typedef vector<ip> pv;
+typedef vector<iv> ivv;
+typedef set<int> ist;
+typedef set<ll> lst;
+typedef map<int, int> imp;
+typedef map<ll, ll> lmp;
+ll gcd(ll a, ll b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+ll digsum(ll n) {
+    ll res = 0;
+    while (n > 0) {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+ll pow(ll x, ll n) {
+    ll ans = 1;
+    while (n) {
+        if (n % 2) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+
+map<ll, ll> numeric_component(ll N) {
+    map<ll, ll> res;
+    for (ll i = 2; i * i <= N; i++) {
+        if (N % i != 0) continue;
+        while (N % i == 0) {
+            res[i]++;
+            N /= i;
+        }
+    }
+    if (N != 1) res[N]++;
+    return res;
+}
+
+iv dx = {-1, 0, 1, 0};
+iv dy = {0, -1, 0, 1};
+
+struct edge {
+    int from, to, length;
+};
+
+void print(iv &a) {
+    for (auto v : a) cout << v << ' ';
+    cout << endl;
+}
+
+int main() {
+    int N, M; cin >> N >> M;
+    iv P(N), L(M), D(M);
+    rep(i, N) cin >> P[i];
+    rep(i, M) cin >> L[i];
+    rep(i, M) cin >> D[i];
+
+    sort(all(P));
+    ll sum = 0;
+    rep(i, N) sum += P[i];
+    priority_queue<ip> LD; rep(i, M) LD.push(make_pair(-L[i], D[i]));
+    sort(all(D));
+
+    // print(P);
+    priority_queue<ip> pq;
+    rep(i, N){
+        // cout << sum << endl;
+        // 使用できるクーポンを追加していく
+        while(!LD.empty()){
+            auto iter = LD.top();
+            if(abs(iter.first) <= P[i]){
+                pq.push(make_pair(iter.second, abs(iter.first)));
+                LD.pop();
+            }
+            else{
+                break;
+            }
+        }
+
+        if(!pq.empty()){
+            auto iter = pq.top(); pq.pop();
+            sum -= iter.first;
+        }
+    }
+    cout << sum << endl;
+}
