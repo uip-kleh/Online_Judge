340235b50d1a79b0e5c3b0dbdd7dba6150ffccc6
uip-kleh <h23ms416@hirosaki-u.ac.jp>
2023-07-30 14:44:42 +0900

    solve

---
+++ AtCoder/ABC/D/310/a.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(ll i = 0; i < ll(n); i++)
+#define nrep(i, n) for(ll i = 1; i < ll(n + 1); i++)
+#define rrep(i, n) for(ll i = ll(n) - 1; i >= 0; i--)
+#define all(x) (x).begin(), (x).end()
+#define SIZE 2e5
+#define INF 2e18
+constexpr long long MOD = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+typedef vector<int> iv;
+typedef vector<ll> lv;
+typedef vector<bool> bv;
+typedef pair<ll, ll> ip;
+typedef vector<ip> pv;
+typedef vector<iv> ivv;
+typedef set<int> ist;
+typedef set<ll> lst;
+typedef map<int, int> imp;
+typedef map<ll, ll> lmp;
+ll gcd(ll a, ll b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+ll digsum(ll n) {
+    ll res = 0;
+    while (n > 0) {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+ll pow(ll x, ll n) {
+    ll ans = 1;
+    while (n) {
+        if (n % 2) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+
+map<ll, ll> numeric_component(ll N) {
+    map<ll, ll> res;
+    for (ll i = 2; i * i <= N; i++) {
+        if (N % i != 0) continue;
+        while (N % i == 0) {
+            res[i]++;
+            N /= i;
+        }
+    }
+    if (N != 1) res[N]++;
+    return res;
+}
+
+iv dx = {-1, 0, 1, 0};
+iv dy = {0, -1, 0, 1};
+
+struct edge {
+    int from, to, length;
+};
+
+void print(iv &a) {
+    for (auto v : a) cout << v << ' ';
+    cout << endl;
+}
+
+int N, T, M;
+iv hate, teams;
+
+int dfs(int now){
+    if(now == N) return size(teams) == T;
+
+    int ans{};
+    for(auto &&team: teams){
+        if(!(team & hate[now])) {
+            team ^= 1 << now;
+            ans += dfs(now + 1);
+            team ^= 1 << now;
+        }
+    }
+
+    if(size(teams) < T){
+        teams.push_back(1 << now);
+        ans += dfs(now + 1);
+        teams.pop_back();
+    }
+    return ans;
+}
+
+int main() {
+    cin >> N >> T >> M;
+    hate = iv(N);
+    for (unsigned i{}, a, b; i < M; ++i) {
+        cin >> a >> b;
+        hate[--b] |= 1U << --a;
+    }
+    teams.reserve(T);
+    cout << dfs(0) << endl;
+}
---
+++ AtCoder/ABC/D/312/a.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+#define rep(i, n) for(ll i = 0; i < ll(n); i++)
+#define nrep(i, n) for(ll i = 1; i < ll(n + 1); i++)
+#define rrep(i, n) for(ll i = ll(n) - 1; i >= 0; i--)
+#define all(x) (x).begin(), (x).end()
+#define SIZE 2e5
+#define INF 2e18
+constexpr long long MOD = 1e9 + 7;
+using namespace std;
+typedef long long ll;
+typedef vector<int> iv;
+typedef vector<ll> lv;
+typedef vector<bool> bv;
+typedef pair<ll, ll> ip;
+typedef vector<ip> pv;
+typedef vector<iv> ivv;
+typedef set<int> ist;
+typedef set<ll> lst;
+typedef map<int, int> imp;
+typedef map<ll, ll> lmp;
+ll gcd(ll a, ll b) {
+    return b ? gcd(b, a % b) : a;
+}
+
+ll lcm(ll a, ll b) {
+    return a / gcd(a, b) * b;
+}
+
+ll digsum(ll n) {
+    ll res = 0;
+    while (n > 0) {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
+
+ll pow(ll x, ll n) {
+    ll ans = 1;
+    while (n) {
+        if (n % 2) ans *= x;
+        x *= x;
+        n >>= 1;
+    }
+    return ans;
+}
+
+map<ll, ll> numeric_component(ll N) {
+    map<ll, ll> res;
+    for (ll i = 2; i * i <= N; i++) {
+        if (N % i != 0) continue;
+        while (N % i == 0) {
+            res[i]++;
+            N /= i;
+        }
+    }
+    if (N != 1) res[N]++;
+    return res;
+}
+
+iv dx = {-1, 0, 1, 0};
+iv dy = {0, -1, 0, 1};
+
+struct edge {
+    int from, to, length;
+};
+
+void print(iv &a) {
+    for (auto v : a) cout << v << ' ';
+    cout << endl;
+}
+
+int main() {
+    string S; cin >> S;
+    ivv dp(2*S.length()+1, iv(S.length()+1, 0));
+
+    if(S[0] == '(' or S[0] == '?') dp[0][0] = 1;
+
+    rep(i, S.length()){
+        rep(j, S.length()){
+            if(S[i] == '('){
+                dp[i+1][j+1] += dp[i][j];
+                dp[i+1][j+1] %= 998244353;
+            }
+            if(S[i] == ')'){
+                if(j-1 < 0) continue;
+                dp[i+1][j-1] += dp[i][j];
+                dp[i+1][j-1] %= 998244353;
+            }
+            if(S[i] == '?'){
+                dp[i+1][j+1] += dp[i][j];
+                dp[i+1][j+1] %= 998244353;
+                if(j-1 < 0) continue;
+                dp[i+1][j-1] += dp[i][j];
+                dp[i+1][j-1] %= 998244353;
+            }
+        }
+    }
+    cout << dp[S.length()][0] << endl;
+    // rep(i, S.length()+1) print(dp[i]);
+}
+
